d9dd00fddec9935705a45791eec5a3d32b34f51d
Matías Mercadé <mimercade@uc.cl>
2022-10-03 19:36:19 -0300

    I'm steadily learning how to use git.

---
+++ main.py
@@ -0,0 +1,6 @@
+# I'm steadily remembering how to code in Python.
+
+print("I'm steadily remembering how to code in Python.")
+print("I'm steadily learning how to use git.")
+print("I'm steadily learning how to use GitHub.")
+print("I'm steadily learning how ti use VSCode.")
