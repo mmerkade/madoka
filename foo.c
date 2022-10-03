4100c0fbcc23b7d158606f4677b6d8f37f437b5e
Matías Mercadé <mimercade@uc.cl>
2022-10-03 19:49:55 -0300

    I'm learning.

---
+++ foo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("I'm steadily remembering how to code in C.\n");
+    }
+    return 0;
+}
